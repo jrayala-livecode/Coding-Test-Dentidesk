3ceb4f92c76c600a366aa34da2ff86b52badd4c9
Joaquín <joaquin.ayala.c@gmail.com>
2023-05-25 15:29:03 -0400

    Validación y mesajes en español

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -45,7 +45,7 @@ class AuthController extends Controller
 
         if (!Auth::attempt($request->only('email', 'password'))) {
             throw ValidationException::withMessages([
-                'email' => 'Invalid credentials',
+                'email' => 'Credenciales inválidas',
             ]);
         }
 
